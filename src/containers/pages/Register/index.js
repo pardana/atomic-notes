1643369ed89b39124e48345e9d7a5e52d12ba764
Aditria Pardana <aditria.pardana20@gmail.com>
2023-01-19 16:26:03 +0700

    create form register

--- src/containers/pages/Register/index.js
+++ src/containers/pages/Register/index.js
@@ -1,15 +1,45 @@
-import "./Register.scss";
 import React, { Component } from "react";
+import "./Register.scss";
 
 class Register extends Component {
+  state = {
+    email: "",
+    password: "",
+  };
+
+  handleChangeText = (e) => {
+    this.setState({
+      [e.target.id]: e.target.value,
+    });
+  };
+
+  handleRegisterSubmit = () => {
+    console.log(this.state.email);
+    console.log(this.state.password);
+  };
+
   render() {
     return (
       <div className="auth-container">
         <div className="auth-card">
           <p className="auth-title">Register Pages</p>
-          <input className="input" type="text" placeholder="Email" />
-          <input className="input" type="password" placeholder="Password" />
-          <button className="btn">Register</button>
+          <input
+            className="input"
+            id="email"
+            type="text"
+            placeholder="Email"
+            onChange={this.handleChangeText}
+          />
+          <input
+            className="input"
+            id="password"
+            type="password"
+            placeholder="Password"
+            onChange={this.handleChangeText}
+          />
+          <button className="btn" onClick={this.handleRegisterSubmit}>
+            Register
+          </button>
         </div>
 
         {/* <button>Go to Dashboard</button> */}
