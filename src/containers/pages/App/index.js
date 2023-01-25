5f8f6e76362b0120d3b2c806c5818da36faa3cbe
Aditria Pardana <aditria.pardana20@gmail.com>
2023-01-25 22:40:16 +0700

    add install redux and react-redux

--- src/containers/pages/App/index.js
+++ src/containers/pages/App/index.js
@@ -4,23 +4,29 @@ import Dashboard from "../Dashboard";
 import Login from "../Login";
 import Register from "../Register";
 
+import { Provider } from "react-redux";
+
+import { store } from "../../../config/redux";
+
 function App() {
   return (
-    <Router>
-      <div>
-        <Switch>
-          <Route path="/login">
-            <Login />
-          </Route>
-          <Route path="/register">
-            <Register />
-          </Route>
-          <Route exact path="/">
-            <Dashboard />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <div>
+          <Switch>
+            <Route path="/login">
+              <Login />
+            </Route>
+            <Route path="/register">
+              <Register />
+            </Route>
+            <Route exact path="/">
+              <Dashboard />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    </Provider>
   );
 }
 
